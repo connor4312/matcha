0f6ab9f66ed1cdf29e27fcf139933130a4be1966
Connor Peet <connor@peet.io>
2019-12-23 06:22:16 -0500

    fix: misleading example benchmark
    https://twitter.com/progdom/status/1208994668706324480

--- example.js
+++ example.js
@@ -1,18 +1,18 @@
-set('maxTime', 1)
+set('maxTime', 1);
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let sum = 0;
 
-bench('forEach', () => arr.forEach(v => sum += v));
+bench('forEach', () => arr.forEach(v => (sum ^= v)));
 
 bench('for-of loop', () => {
   for (const v of arr) {
-    sum += v;
+    sum ^= v;
   }
 });
 
 bench('for loop', () => {
   for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
+    sum ^= arr[i];
   }
 });
